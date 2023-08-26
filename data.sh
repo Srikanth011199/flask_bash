fbfa8103bea3cc80ae0e6896c1f233a6b28c94ed
Srikanth011199 <140722653+Srikanth011199@users.noreply.github.com>
2023-08-26 22:25:51 +0530

    data.sh

--- data.sh
+++ data.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y python3-pip
 sudo git clone https://github.com/Srikanth011199/flask-library-app.git
 cd flask-library-app/
 pip3 install -r requirements.txt
-python3 app.py
+nohup -u ./app.py
